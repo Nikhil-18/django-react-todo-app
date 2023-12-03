0364dada51caa4d0c6192ecb0e0677ee8d761a3a
Nikhil Bhaskar <nik18b@gmail.com>
2023-12-03 21:19:31 +0530

    Redesign with bootstrap; add status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="container">
       <NewTodo handleAddTodo={handleAddTodo} />
       <TodoList
         todoList={todoList}
--- frontend/src/components/NewTodo.js
+++ frontend/src/components/NewTodo.js
@@ -5,6 +5,7 @@ const NewTodo = ({ handleAddTodo }) => {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
+    status: "To Do",
   });
 
   const handleFormChange = (e) => {
@@ -14,24 +15,65 @@ const NewTodo = ({ handleAddTodo }) => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    handleAddTodo(formData)
+    handleAddTodo(formData);
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <input
-        name="title"
-        type="text"
-        value={formData.title}
-        onChange={handleFormChange}
-      />
-      <textarea
-        name="description"
-        value={formData.description}
-        onChange={handleFormChange}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="col col-md-6 mx-auto my-3">
+      <form onSubmit={handleFormSubmit}>
+        <div class="mb-3">
+          <label for="titleInput" class="form-label">
+            Title
+          </label>
+          <input
+            name="title"
+            type="text"
+            value={formData.title}
+            onChange={handleFormChange}
+            required
+            class="form-control"
+            id="titleInput"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="descInput" class="form-label">
+            Description
+          </label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleFormChange}
+            class="form-control"
+            id="descInput"
+          />
+        </div>
+        <div class="col-md-3">
+          <label for="validationCustom04" class="form-label">
+            Status
+          </label>
+          <select
+            name="status"
+            class="form-select"
+            onChange={handleFormChange}
+            id="validationCustom04"
+            required
+          >
+            <option selected value="To Do">
+              To Do
+            </option>
+            <option value="In Progress">In Progress</option>
+            <option value="Done">Done</option>
+          </select>
+          <div class="invalid-feedback">Please select a valid state.</div>
+        </div>
+
+        <div class="d-grid gap-2 col-6 mx-auto">
+          <button type="submit" class="btn btn-primary my-3">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
