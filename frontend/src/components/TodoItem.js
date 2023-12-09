307bb5202e491f591466e662efb439d02a90aaa3
Nikhil Bhaskar <nik18b@gmail.com>
2023-12-09 23:24:09 +0530

    update status of old todos

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TodoItem = ({ todo, handleUpdate, handleDelete }) => {
   const { id, title, description, status } = todo;
+
+  const [selectedStatus, setSelectedStatus] = useState(status);
 
   let borderColor = "";
   if (status === "In Progress") {
@@ -13,19 +15,56 @@ const TodoItem = ({ todo, handleUpdate, handleDelete }) => {
 
   const headerColor = status === "Done" ? "text-success" : "";
 
+  const handleOptionChange = (e) => {
+    setSelectedStatus(e.target.value);
+  };
+
+  const handleUpdateButton = () => {
+    if (status === selectedStatus) return;
+    const modTodo = { title, description, status: selectedStatus };
+    handleUpdate(id, modTodo);
+  };
+
   return (
     <div className="col">
       <div className={`card ${borderColor}`}>
         <div className="card-body">
           <h5 className={`card-title ${headerColor}`}>{title}</h5>
           <p className="card-text">{description}</p>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={() => handleDelete(id)}
-          >
-            Delete
-          </button>
+          <div className="d-flex flex-row bd-highlight justify-content-between">
+            <div className="row">
+              <div className="col">
+                <select
+                  name="status"
+                  className="form-select"
+                  onChange={handleOptionChange}
+                  id="validationCustom04"
+                  required
+                  // defaultValue={status}
+                  value={selectedStatus}
+                >
+                  <option value="To Do">To Do</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Done">Done</option>
+                </select>
+              </div>
+              <div className="col">
+                <button
+                  className="btn btn-warning"
+                  onClick={handleUpdateButton}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={() => handleDelete(id)}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
